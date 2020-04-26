02b814bc6138a78963c33c18b722ac98360b9d7e
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2020-04-26 14:47:03 +0430

    now we can get book picture by ISBN

--- src/Ketabir.php
+++ src/Ketabir.php
@@ -2,20 +2,74 @@
 
 namespace BookByISBN;
 
+use GuzzleHttp\Client;
+
 class Ketabir
 {
-    public function get_book_address_by_isbn($argument1)
+
+
+    private function make_my_dirty_header($_ISBN)
     {
-        $opts = array(
-            'http' => array(
-                'method' => "POST",
-                'header' => "content-type: application/x-www-form-urlencoded"
-            )
+        $content = file_get_contents("http://ketab.ir/Search.aspx", false);
+        preg_match("/id=\"__VIEWSTATE\" value=\"(.*)\" \/>/", $content, $VIEWSTATE);
+        $view_state = $VIEWSTATE[1];
+        preg_match("/id=\"__VIEWSTATEGENERATOR\" value=\"(.*)\" \/>/", $content, $VIEWSTATEGENERATOR);
+        $view_state_generator = $VIEWSTATEGENERATOR[1];
+        preg_match("/id=\"__EVENTVALIDATION\" value=\"(.*)\" \/>/", $content, $EVENTVALIDATION);
+        $event_validation = $EVENTVALIDATION[1];
+
+        $body = [
+            "__VIEWSTATE" => $view_state,
+            "__VIEWSTATEGENERATOR" => $view_state_generator,
+            "__EVENTVALIDATION" => $event_validation,
+            "ctl00\$SiteMenu\$Search\$DropDownFieldList" => "1",
+            "ctl00\$SiteMenu\$Search\$DDLTypeSearch" => "1",
+            "ctl00\$ContentPlaceHolder1\$TxtIsbn" => $_ISBN,
+            "ctl00\$ContentPlaceHolder1\$drpDewey" => "-1",
+            "ctl00\$ContentPlaceHolder1\$drpFromIssueYear" => "57",
+            "ctl00\$ContentPlaceHolder1\$drpFromIssueMonth" => "01",
+            "ctl00\$ContentPlaceHolder1\$drpFromIssueDay" => "01",
+            "ctl00\$ContentPlaceHolder1\$drpToIssueYear" => "98",
+            "ctl00\$ContentPlaceHolder1\$drpToIssueMonth" => "12",
+            "ctl00\$ContentPlaceHolder1\$drpToIssueDay" => "30",
+            "ctl00\$ContentPlaceHolder1\$drLanguage" => "0",
+            "ctl00\$ContentPlaceHolder1\$DrSort" => "1",
+            "ctl00\$ContentPlaceHolder1\$DrPageSize" => "100",
+            "ctl00\$ContentPlaceHolder1\$BtnSearch" => "",
+        ];
+        return $body;
+    }
+
+    public function get_book_picture_by_isbn($_ISBN)
+    {
+        $body = $this->make_my_dirty_header($_ISBN);
+        $http_content = http_build_query($body);
+        $client = new Client(['cookies' => true]);
+        $response = $client->request(
+            'POST',
+            'http://ketab.ir/Search.aspx',
+            [
+                'form_params' => $body,
+                'allow_redirects' => true,
+                'headers' => [
+                    "content-type: application/x-www-form-urlencoded",
+                    "Content-Length: " . strlen($http_content)
+                ]
+            ]
         );
+        if ($response->getStatusCode() === 200) {
+            $find_page_content = $response->getBody()->getContents();
+            return $this->give_me_picture($find_page_content);
+        }
+        return false;
+    }
 
-        $context = stream_context_create($opts);
-
-        // Open the file using the HTTP headers set above
-        $file = file_get_contents('http://ketab.ir/Search.aspx', false, $context);
+    private function give_me_picture($_page_content)
+    {
+        preg_match("/href=\"\/bookview.aspx\?bookid=(.*)\"><img id=\"(.*)\" src=\"(.*)\" height=\"100\"/", $_page_content, $DATAPERRESULT);
+        if (!isset($DATAPERRESULT[3]))
+            return false;
+        var_dump($DATAPERRESULT[3]);
+        return $DATAPERRESULT[3];
     }
 }
