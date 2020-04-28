f250234af59d44673cb09c645f6cb06bfe638057
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2020-04-28 13:20:19 +0430

    we must to use right ISBN for books (some test)

--- spec/KetabirSpec.php
+++ spec/KetabirSpec.php
@@ -14,7 +14,7 @@ class KetabirSpec extends ObjectBehavior
 
     public function it_can_get_book_picture_by_isbn()
     {
-        $this->get_book_picture_by_isbn("9789643113445")->shouldContain("http://164.138.18.205/DataBase/BookImages");
+        $this->get_book_picture_by_isbn("964-2793-03-2")->shouldContain("http://164.138.18.205/DataBase/BookImages");
         $this->get_book_picture_by_isbn("9789646235793")->shouldContain("http://164.138.18.205/DataBase/BookImages");
         $this->get_book_picture_by_isbn("9786002571755")->shouldContain("http://164.138.18.205/DataBase/BookImages");
     }
--- spec/OpacNlaiSpec.php
+++ spec/OpacNlaiSpec.php
@@ -14,6 +14,6 @@ class OpacNlaiSpec extends ObjectBehavior
 
     function it_can_get_book_detail_by_ISBN()
     {
-        $this->get_book_detail("9789646235793")->shouldBeArray();
+        $this->get_book_detail("964-2793-03-2")->shouldBeArray();
     }
 }
