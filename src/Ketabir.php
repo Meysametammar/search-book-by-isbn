d1330c584edf0d9175fdc558c551a4fd6b5e1938
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2020-10-03 20:28:12 +0330

    uncomment get book detail

--- src/Ketabir.php
+++ src/Ketabir.php
@@ -19,10 +19,9 @@ class Ketabir
      */
     public $book_detail;
 
-
     /**
      * @param mixed $_ISBN
-     * 
+     *
      * @return void
      */
     public function __construct($_ISBN)
@@ -30,10 +29,9 @@ class Ketabir
         $this->make_everything_ready($_ISBN);
     }
 
-
     /**
      * @param mixed $_ISBN
-     * 
+     *
      * @return void
      */
     private function make_my_dirty_header($_ISBN)
@@ -48,16 +46,23 @@ class Ketabir
         $content = $response->getBody()->getContents();
         // $content = file_get_contents("http://ketab.ir/Search.aspx", false);
         preg_match("/id=\"__VIEWSTATE\" value=\"(.*)\" \/>/", $content, $VIEWSTATE);
-        if (!isset($VIEWSTATE[1]))
+        if (!isset($VIEWSTATE[1])) {
             return false;
+        }
         $view_state = $VIEWSTATE[1];
-        preg_match("/id=\"__VIEWSTATEGENERATOR\" value=\"(.*)\" \/>/", $content, $VIEWSTATEGENERATOR);
-        if (!isset($VIEWSTATEGENERATOR[1]))
+        preg_match(
+            "/id=\"__VIEWSTATEGENERATOR\" value=\"(.*)\" \/>/",
+            $content,
+            $VIEWSTATEGENERATOR
+        );
+        if (!isset($VIEWSTATEGENERATOR[1])) {
             return false;
+        }
         $view_state_generator = $VIEWSTATEGENERATOR[1];
         preg_match("/id=\"__EVENTVALIDATION\" value=\"(.*)\" \/>/", $content, $EVENTVALIDATION);
-        if (!isset($EVENTVALIDATION[1]))
+        if (!isset($EVENTVALIDATION[1])) {
             return false;
+        }
         $event_validation = $EVENTVALIDATION[1];
 
         $body = [
@@ -82,10 +87,9 @@ class Ketabir
         return $body;
     }
 
-
     /**
      * @param mixed $_ISBN
-     * 
+     *
      * @return void
      */
     private function make_everything_ready($_ISBN)
@@ -93,30 +97,25 @@ class Ketabir
         $body = $this->make_my_dirty_header($_ISBN);
         $http_content = http_build_query($body);
         $client = new Client(['cookies' => true]);
-        $response = $client->request(
-            'POST',
-            'http://ketab.ir/Search.aspx',
-            [
-                'form_params' => $body,
-                'allow_redirects' => true,
-                'headers' => [
-                    "content-type: application/x-www-form-urlencoded",
-                    "Content-Length: " . strlen($http_content)
-                ]
-            ]
-        );
+        $response = $client->request('POST', 'http://ketab.ir/Search.aspx', [
+            'form_params' => $body,
+            'allow_redirects' => true,
+            'headers' => [
+                "content-type: application/x-www-form-urlencoded",
+                "Content-Length: " . strlen($http_content),
+            ],
+        ]);
         if ($response->getStatusCode() !== 200) {
             return false;
         }
         $find_page_content = $response->getBody()->getContents();
         $this->give_me_picture($find_page_content);
-        // $this->give_book_detail($this->book_address);
+        $this->give_book_detail($this->book_address);
     }
-
 
     /**
      * @param mixed $_page_url
-     * 
+     *
      * @return void
      */
     private function give_book_detail($_page_url)
@@ -125,35 +124,38 @@ class Ketabir
         $client = new Client([
             'cookies' => true,
         ]);
-        $response = $client->get($_page_url, ['verify' => true,]);
+        $response = $client->get($_page_url, ['verify' => true]);
         if ($response->getStatusCode() !== 200) {
             return false;
         }
         $content = $response->getBody()->getContents();
         // var_dump($content);
         preg_match("/<span id=\"(.*)\" class=\"h4\">(.*)<\/span>/", $content, $TITLE);
-        $book_detail["title"] = (isset($TITLE[2])) ? $TITLE[2] : null;
+        $book_detail["title"] = isset($TITLE[2]) ? $TITLE[2] : null;
 
         preg_match("/<span id=\"(.*)\" class=\"h4\">(.*)<\/span>/", $content, $TITLE);
-        $book_detail["title"] = (isset($TITLE[2])) ? $TITLE[2] : null;
-
+        $book_detail["title"] = isset($TITLE[2]) ? $TITLE[2] : null;
 
         $this->book_detail = $book_detail;
         // var_dump($book_detail);
     }
 
-
     /**
      * @param mixed $_page_content
      * @param mixed $book_id
-     * 
+     *
      * @return void
      */
     private function give_me_picture($_page_content)
     {
-        preg_match("/href=\"\/bookview.aspx\?bookid=(.*)\"><img id=\"(.*)\" src=\"(.*)\" height=\"100\"/", $_page_content, $DATAPERRESULT);
-        if (!isset($DATAPERRESULT[3]))
+        preg_match(
+            "/href=\"\/bookview.aspx\?bookid=(.*)\"><img id=\"(.*)\" src=\"(.*)\" height=\"100\"/",
+            $_page_content,
+            $DATAPERRESULT
+        );
+        if (!isset($DATAPERRESULT[3])) {
             return false;
+        }
 
         $this->book_address = "http://ketab.ir/bookview.aspx?bookid=" . $DATAPERRESULT[1];
         $this->book_picture = $DATAPERRESULT[3];
